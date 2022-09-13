7b9efc9f7c7e3ef5d8a888edead82c0a05f16727
tebogo184 <tebogotshukudu184@gmail.com>
2022-09-13 23:23:08 +0200

    I have added three

--- Informatics 1b(team 33)/region.vb
+++ Informatics 1b(team 33)/region.vb
@@ -1,7 +1,9 @@
 ﻿Public Class region
     Private _name As String
     Private _landform As String
-
+    Private _size As Double
+    Private _population As Integer
+    Private _nfacilities As Integer
     Public Property name As String
         Set(value As String)
             _name = name
@@ -17,5 +19,32 @@
         Get
             Return _landform
         End Get
+    End Property
+
+    Public Property size As Double
+        Get
+            Return _size
+        End Get
+        Set(value As Double)
+            _size = value
+        End Set
+    End Property
+
+    Public Property poplulation As Integer
+        Get
+            Return _population
+        End Get
+        Set(value As Integer)
+            _population = value
+        End Set
+    End Property
+
+    Public Property nfacilities As Integer
+        Get
+            Return _nfacilities
+        End Get
+        Set(value As Integer)
+            _nfacilities = value
+        End Set
     End Property
 End Class
